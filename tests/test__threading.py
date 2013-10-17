7306483c09edd95edc78e6353b5afe371f52a2a5
Nick Bargnesi <nbargnesi@selventa.com>
2013-10-17 19:23:09 -0400

    improve thread-based unittest robustness
    Use synch points to ensure the poll interval change is effective.

--- tests/test__threading.py
+++ tests/test__threading.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import time
 import unittest
+import threading
 from datetime import datetime
 from lazarus import _threading
 
@@ -58,10 +59,16 @@ class TestPollThread(unittest.TestCase):
     def test_change_interval(self):
         '''Test changing a PollThread interval'''
 
-        def wait1():
-            time.sleep(1)
+        step1 = threading.Event()
+        step2 = threading.Event()
 
-        pt = _threading.PollThread(wait1, poll_interval=0)
+        def wait1():
+            step1.set()
+            if not step2.wait(5):
+                self.fail('timeout: failed waiting for sync point')
+            step2.clear()
+
+        pt = _threading.PollThread(wait1, poll_interval=0.01)
 
         # poll thread constructed as daemon
         self.assertTrue(pt.isDaemon())
@@ -71,25 +78,41 @@ class TestPollThread(unittest.TestCase):
 
         # start the thread
         pt.start()
-        # note the current datetime t0
-        t0 = datetime.now()
         # assert it's alive
         self.assertTrue(pt.isAlive())
 
-        # change its interval to 1 seconds
-        _threading.change_poll_interval(1)
+        # wait until thread arrives
+        if not step1.wait(5):
+            self.fail('timeout: failed waiting for sync point')
+        # reset while thread waits
+        step1.clear()
+
+        # change its interval to 0.25 seconds
+        _threading.change_poll_interval(0.25)
+
+        # note the current datetime t0
+        t0 = datetime.now()
+
+        # let thread continue
+        step2.set()
+        # wait until thread arrives
+        if not step1.wait(5):
+            self.fail('timeout: failed waiting for sync point')
+        # reset while thread waits (not necessary)
+        step1.clear()
+
+        # note the current datetime t0
+        t1 = datetime.now()
 
         # stop the thread and join it
         pt.stop()
+        step2.set()
         pt.join()
-
-        # note the current datetime t1
-        t1 = datetime.now()
 
         # the time delta (t1 - t0) should be > 2 seconds
         delta = (t1 - t0).total_seconds()
         # (sleep in the poll function plus the interval)
-        self.assertGreater(delta, 2, 'the delta was too small (%s)' % delta)
+        self.assertGreater(delta, 0.25, 'the delta was too small (%s)' % delta)
 
         # assert it is dead
         self.assertFalse(pt.isAlive())
