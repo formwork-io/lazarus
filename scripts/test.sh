dac1b4bec404612d17ddde974f2ca9bf45f88450
Nick Bargnesi <nbargnesi@selventa.com>
2013-10-22 19:52:42 -0400

    disable xunit output from nose

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 cd "${DIR}" || exit 1
 
-NOSE_OPTS="--with-xunit "
+#NOSE_OPTS="--with-xunit "
 NOSE_OPTS+="--nocapture "
 NOSE_OPTS+="--with-doctest "
 NOSE_OPTS+="--doctest-options=+ELLIPSIS "
