4a8453b6976b162d4cc10d096f1e74d9774cef5b
Nick Bargnesi <nick@den-4.com>
2013-10-27 22:33:22 -0400

    towards #8

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,6 +4,6 @@
 #
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 cd "${DIR}" || exit 1
-coverage run --source=lazarus setup.py test || exit 1
+./scripts/test.sh || exit 1
 coverage report -m || exit 1
 
--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,9 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 cd "${DIR}" || exit 1
 
+NOSE_OPTS+="--with-coverage "
+NOSE_OPTS+="--cover-package=lazarus "
+NOSE_OPTS+="--cover-erase "
 NOSE_OPTS+="--nocapture "
 NOSE_OPTS+="--with-doctest "
 NOSE_OPTS+="--doctest-options=+ELLIPSIS "
