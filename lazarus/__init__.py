a59c304f8c275fb28bf4e9ee98fb7ec9c0ad6a29
Nick Bargnesi <nick@den-4.com>
2014-11-13 22:01:35 -0500

    version to 0.5.1

--- lazarus/__init__.py
+++ lazarus/__init__.py
@@ -5,7 +5,7 @@ Progress doesn't come from early risers - progress is made by lazy men looking
 for easier ways to do things.
 '''
 
-__version__ = '0.6-dev'
+__version__ = '0.5.1'
 import os
 import sys
 from . import _util
